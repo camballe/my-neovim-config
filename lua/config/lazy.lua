d3a4d19257ffba0bbaac99b0304094b1af3a803f
Enoch Kambale <campnoch@gmail.com>
2024-10-12 19:15:12 +0300

    feat: latest config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "sonokai",
+        colorscheme = "solarized-osaka",
       },
     },
     -- import/override with your plugins
@@ -43,7 +43,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "sonokai" } },
+  install = { colorscheme = { "solarized-osaka" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
